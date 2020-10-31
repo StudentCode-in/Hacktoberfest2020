e8a49bfeec8f706de8ff3ef633644cba3530f7bd
jcybertech <jcyber2000@gmail.com>
2020-10-30 23:02:05 -0600

    Made an option to allow user to exit game gracefully as a staticmethod.

--- Programs/guess_number.py
+++ Programs/guess_number.py
@@ -4,27 +4,40 @@ In this game, user input a number and then computer generate a random number.
 If numbers are equal then you win else loss.
 """
 import random
+import sys
 
 
 class GuessNumber:
     """GuessNumber"""
+    @staticmethod
+    def exit_game():
+        sys.exit()
 
     def user_input(self):
         """Method to take input from user."""
-        self.num = int(input("Guess a number: "))
-        return self.num
+        self.num = input("Guess a number: ")
+        if self.num == int:
+            return self.num
+        else:
+            if self.num == str:
+                return self.num 
 
     def computer_output(self):
         """Method to generate random number and checks for the equality."""
         self.random_number = random.randint(1, 100)
         if self.num == self.random_number:
             print("Hurray! You won and number is {}".format(self.num))
+        elif self.num == "exit" or "e":
+            guess.exit_game()
         else:
             print("Please try again")
 
 
 # Driver code
-while True:
+# while True:
+if __name__ == "__main__":
     guess = GuessNumber()
     guess.user_input()
     guess.computer_output()
+    guess.exit_game()
+
